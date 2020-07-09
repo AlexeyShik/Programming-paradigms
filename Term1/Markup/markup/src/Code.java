f2772f792c12cfbab87f3af5419ca57fe40b6ca0
Alexey Shik <58121508+AlexeyShik@users.noreply.github.com>
2020-07-10 05:10:43 +0600

    Delete Code.java

--- Term1/Markup/markup/src/Code.java
+++
@@ -1,8 +0,0 @@
-import markUp.*;
-import java.util.*;
-
-public class Code {
-    public static void main(String[] args) {
-        //Paragraph p = new Paragraph(new OrderedList(List.of(new ListItem(List.of(new Paragraph(List.of(new Text("3"))))))));
-    }
-}
